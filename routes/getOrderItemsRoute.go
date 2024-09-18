8e0c5e7fdeb5794d4e00b39983d14a10c0a94655
nuredinbedruyimer <nuredinbedru06@gmail.com>
2024-09-18 23:54:00 +0300

    All Routes Are Created

---
+++ routes/getOrderItemsRoute.go
@@ -0,0 +1,19 @@
+package routes
+
+import "github.com/gin-gonic/gin"
+
+func NoteRoutes(noteRoute *gin.Engine) {
+	//  Get List Of OrderItems
+	noteRoute.GET("/orderItems", controller.GetOrderItems())
+	//  Get Single OrderItems
+	noteRoute.GET("/orderItems/:orderItem_id", controller.GetOrderItem())
+	//  Get Order Items Usig Order
+	noteRoute.GET("/orderItems/:order_id", controller.GetOrderItemsByOrder())
+
+	//  Update OrderItems using Patch
+	noteRoute.PATCH("/orderItems/:orderItem_id", controller.UpdateOrderItem())
+
+	//  Create OrderItems
+	noteRoute.POST("/orderItems", controller.CreateOrderItem())
+
+}
